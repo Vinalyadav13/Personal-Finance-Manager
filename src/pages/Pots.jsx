e9877d8f79a648001a51213b74aea1bf76fda543
Vinalyadav13 <vinalyadav013@gmail.com>
2026-06-19 14:22:58 +0530

    Update Pots.jsx

--- src/pages/Pots.jsx
+++ src/pages/Pots.jsx
@@ -23,6 +23,12 @@ const [moneyAmount, setMoneyAmount] =
 const [withdrawAmount, setWithdrawAmount] =
   useState("");
 
+  const [showDeleteModal, setShowDeleteModal] =
+  useState(false);
+
+const [deleteId, setDeleteId] =
+  useState(null);
+
 
   const [showModal, setShowModal] = useState(false);
 
@@ -209,6 +215,11 @@ const handleWithdrawMoney = async () => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const openDeleteModal = (id) => {
+  setDeleteId(id);
+  setShowDeleteModal(true);
 };
 
 
@@ -418,7 +429,9 @@ className="
 </button>
 
     <button
-  onClick={() => handleDelete(pot.id)}
+  onClick={() =>
+  openDeleteModal(pot.id)
+}
   className="
   p-2
   rounded-lg
@@ -683,6 +696,56 @@ className="
 
   </div>
 
+)}
+
+{showDeleteModal && (
+  <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+
+    <div className="bg-white p-6 rounded-xl w-[400px]">
+
+      <h2 className="text-xl font-bold mb-3">
+        Delete Pot
+      </h2>
+
+      <p className="text-gray-600 mb-5">
+        Are you sure you want to delete it?
+      </p>
+
+      <div className="flex justify-end gap-3">
+
+        <button
+          onClick={() =>
+            setShowDeleteModal(false)
+          }
+          className="
+            px-4 py-2
+            border
+            rounded-lg
+          "
+        >
+          Cancel
+        </button>
+
+        <button
+          onClick={() => {
+            handleDelete(deleteId);
+            setShowDeleteModal(false);
+          }}
+          className="
+            bg-red-500
+            text-white
+            px-4 py-2
+            rounded-lg
+          "
+        >
+          Delete
+        </button>
+
+      </div>
+
+    </div>
+
+  </div>
 )}
 
     </div>
